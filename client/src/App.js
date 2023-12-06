83702c1bb9d3e337b0eac495c6f20b73bf7f2d0d
paras verma <parasverma2531@gmail.com>
2023-12-06 21:33:26 +0530

    images added

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,38 @@ import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/Signup";
 import OnlyIfNotLoggedIn from "./components/OnlyIfNotLoggedIn";
 import RequireUser from "./components/RequireUser";
+import toast, { Toaster } from "react-hot-toast";
+export const TOAST_SUCCESS = "toast_success";
+export const TOAST_FAILURE = "toast_failure";
 function App() {
+  //for toast
+  useEffect(() => {
+    switch (toastData.type) {
+      case TOAST_SUCCESS:
+        toast.success(toastData.message);
+        break;
+      case TOAST_FAILURE:
+        toast.error(toastData.message);
+        break;
+    }
+  }, [toastData]);
   return (
-    <div className="App">
-      <Routes>
-        <Route element={<RequireUser />}>
-          <Route path="/" element={<Home/>} />
-        </Route>
-        <Route element={<OnlyIfNotLoggedIn />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Route>
-      </Routes>
-    </div>
+    <>
+      <div>
+        <Toaster />
+      </div>
+      <div className="App">
+        <Routes>
+          <Route element={<RequireUser />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+          <Route element={<OnlyIfNotLoggedIn />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Route>
+        </Routes>
+      </div>
+    </>
   );
 }
 
